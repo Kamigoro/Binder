5cf235535810bceecb70f37e95c01452f3711c2b
Dylan Piette <dylan-piette@hotmail.fr>
2020-10-26 12:30:37 +0100

    Creating basic states

---
+++ Sensy.Binder/Sensy.Binder.Domain/States/ConnexionCheckState.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sensy.Binder.Domain.States
+{
+    public class ConnexionCheckState : IState
+    {
+        public string DisplayName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public void Run()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Stop()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Sensy.Binder/Sensy.Binder.Domain/States/IState.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sensy.Binder.Domain.States
+{
+    public interface IState
+    {
+        public string DisplayName { get; set; }
+        void Run();
+        void Stop();
+    }
+}
