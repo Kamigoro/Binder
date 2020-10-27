f5099ceaf64ba2d0ff3521c1204870accfab03b6
Dylan Piette <dylan-piette@hotmail.fr>
2020-10-27 08:38:49 +0100

    Implement interface concrete behaviour Still have to do the data retrieving

--- Sensy.Binder/Sensy.Binder.Domain/Devices/Binder.cs
+++ Sensy.Binder/Sensy.Binder.Domain/Devices/Binder.cs
@@ -2,6 +2,8 @@
 using Sensy.Binder.Domain.Devices.Enums;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,29 +11,84 @@ namespace Sensy.Binder.Domain.Devices
 {
     public class Binder : IChamberDevice
     {
-        public Task<DeviceStatus> IsConnectedAsync()
+        public string IPAddress { get; set; }
+        private const double MaximumTemperature = 180;
+        private const double MinimumTemperature = -40;
+        private const double MaximumHumidity = 100;
+        private const double MinimumHumidity = 0;
+
+        public Binder(string ipAddress)
         {
+            IPAddress = ipAddress;
+        }
+
+        public async Task<DeviceStatus> IsConnectedAsync()
+        {
+            Ping ping = new Ping();
+            PingReply pingReply = await ping.SendPingAsync(IPAddress);
+            if (pingReply.Status == IPStatus.Success)
+            {
+                return DeviceStatus.Connected;
+            }
+            else
+            {
+                return DeviceStatus.Disconnected;
+            }
+        }
+
+        public async Task SetIdleAsync()
+        {
+            await ExecutePythonCommand("-I");
+        }
+
+        public async Task SetHumidityAsync(double humidity)
+        {
+            if (humidity <= MaximumHumidity && humidity >= MinimumHumidity)
+            {
+                await ExecutePythonCommand($"-H {humidity.ToString("0.0###")}");
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException(nameof(humidity), humidity, $"Humidity should be between {MinimumHumidity}% and {MaximumHumidity}%");
+            }
+        }
+
+        public async Task SetTemperatureAsync(double temperature)
+        {
+            if (temperature <= MaximumTemperature && temperature >= MinimumTemperature)
+            {
+                await ExecutePythonCommand($"-T {temperature.ToString("0.0###")}");
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException(nameof(temperature), temperature, $"Humidity should be between {MinimumTemperature}°C and {MaximumTemperature}°C");
+            }
+        }
+
+        public async Task<ChamberData> GetChamberDataAsync()
+        {
+            // TODO Implement chamber data retrieving
             throw new NotImplementedException();
         }
 
-        public Task<ChamberData> GetChamberDataAsync()
+        /// <summary>
+        /// Execute the python script controlling the chamber with specified parameters and force the command.
+        /// </summary>
+        /// <param name="parameters">Tmperature or humidity percentage.</param>
+        /// <returns></returns>
+        private async Task ExecutePythonCommand(string parameters)
         {
-            throw new NotImplementedException();
+            await Task.Run(() =>
+            {
+                Process process = new Process();
+                ProcessStartInfo startInfo = new ProcessStartInfo();
+                startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                startInfo.FileName = "cmd.exe";
+                startInfo.Arguments = $"/C python {Settings.PythonScriptPath} -A {IPAddress} {parameters} -f";
+                process.StartInfo = startInfo;
+                process.Start();
+            });
         }
 
-        public Task SetHumidityAsync(double temperature)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task SetIdleAsync()
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task SetTemperatureAsync(double temperature)
-        {
-            throw new NotImplementedException();
-        }
     }
 }
