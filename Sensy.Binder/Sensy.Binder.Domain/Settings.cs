65f40ddc6701c664082eae9a57f5ddca727a065b
Dylan Piette <dylan-piette@hotmail.fr>
2020-10-27 09:01:17 +0100

    Changing from static class to singleton

--- Sensy.Binder/Sensy.Binder.Domain/Settings.cs
+++ Sensy.Binder/Sensy.Binder.Domain/Settings.cs
@@ -4,8 +4,28 @@ using System.Text;
 
 namespace Sensy.Binder.Domain
 {
-    public static class Settings
+    public class Settings
     {
+        private static readonly Settings instance = new Settings();
+
+        // Explicit static constructor to tell C# compiler
+        // not to mark type as beforefieldinit
+        static Settings()
+        {
+        }
+
+        private Settings()
+        {
+        }
+
+        public static Settings Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+
         public static string PythonScriptPath { get; set; }
         public static string ChamberIPAddress { get; set; }
         public static string MeasurementDeviceComPort { get; set; }
