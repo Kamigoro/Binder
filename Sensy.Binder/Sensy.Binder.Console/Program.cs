d0582bf49f153b73baa4461f9f2bad5459b920bf
Dylan Piette <dylan-piette@hotmail.fr>
2020-10-27 09:04:49 +0100

    Adding console app to test functionnalities

---
+++ Sensy.Binder/Sensy.Binder.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sensy.Binder.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
