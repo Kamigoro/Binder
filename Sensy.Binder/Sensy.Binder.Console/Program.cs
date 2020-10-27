d5122b70268c2c0c474fec95e288efd208b6c553
Dylan Piette <dylan-piette@hotmail.fr>
2020-10-27 13:49:42 +0100

    Testing chamber data acquisition in console app

--- Sensy.Binder/Sensy.Binder.Console/Program.cs
+++ Sensy.Binder/Sensy.Binder.Console/Program.cs
@@ -1,12 +1,24 @@
-﻿using System;
+﻿using Sensy.Binder.Domain;
+using Sensy.Binder.Domain.Devices;
+using System;
+using System.Text;
+using System.Threading;
 
-namespace Sensy.Binder.Console
+namespace Sensy.Binder
 {
     class Program
     {
-        static void Main(string[] args)
+        static async System.Threading.Tasks.Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Settings.PythonScriptPath = @"PythonScript/Binder.py";
+            IChamberDevice binder = new Domain.Devices.Binder("192.168.0.50");
+            var result = await binder.GetChamberDataAsync();
+            Console.WriteLine(result.Note);
+            Console.WriteLine(result.Mode);
+            Console.WriteLine(result.Door);
+            Console.WriteLine(result.Humidity);
+            Console.WriteLine(result.Temperature);
+            Console.WriteLine(result.TargetTemperature);
         }
     }
 }
