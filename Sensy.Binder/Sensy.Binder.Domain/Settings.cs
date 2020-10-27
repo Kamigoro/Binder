32e0bb7d4f0074627cc0222ff781017be224c22d
Dylan Piette <dylan-piette@hotmail.fr>
2020-10-27 08:37:54 +0100

    Adding static class contaning all the setting of the software

---
+++ Sensy.Binder/Sensy.Binder.Domain/Settings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sensy.Binder.Domain
+{
+    public static class Settings
+    {
+        public static string PythonScriptPath { get; set; }
+        public static string ChamberIPAddress { get; set; }
+        public static string MeasurementDeviceComPort { get; set; }
+        public static string ReportModelPath { get; set; }
+        public static string ReportsDirectory { get; set; }
+        public static string StepsDirectory { get; set; }
+    }
+}
