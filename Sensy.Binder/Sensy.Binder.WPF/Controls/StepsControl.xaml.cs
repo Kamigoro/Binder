40e158c4468559eabe21f0b7f6408059d2c0966c
Dylan Piette <dylan-piette@hotmail.fr>
2020-10-29 12:50:18 +0100

    First attempt to make the GUI

---
+++ Sensy.Binder/Sensy.Binder.WPF/Controls/StepsControl.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Sensy.Binder.WPF.Controls
+{
+    /// <summary>
+    /// Logique d'interaction pour StepsControl.xaml
+    /// </summary>
+    public partial class StepsControl : UserControl
+    {
+        public StepsControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
